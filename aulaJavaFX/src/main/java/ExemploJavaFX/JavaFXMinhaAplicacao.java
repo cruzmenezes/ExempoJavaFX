82896b3d5a871345fe2ce0d552171cc9376a6604
cruzmenezes <cruz.menezes@outlook.com>
2025-08-14 18:39:03 -0300

    testando aplicação

--- aulaJavaFX/src/main/java/ExemploJavaFX/JavaFXMinhaAplicacao.java
+++ aulaJavaFX/src/main/java/ExemploJavaFX/JavaFXMinhaAplicacao.java
@@ -1,17 +1,25 @@
 package ExemploJavaFX;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class JavaFXMinhaAplicacao extends Application{
+/**
+ * Exemplo simples de aplicação JavaFX.
+ * Exibe uma janela (Stage) com um título.
+ */
+public class JavaFXMinhaAplicacao extends Application {
 
     @Override
-    public void start(Stage palco){
-        palco.setTitle("meu primeiro palco");
-        palco.show();
+    public void start(Stage palco) {
+        // Define o título da janela
+        palco.setTitle("Meu Primeiro Palco");
 
+        // Exibe a janela
+        palco.show();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        // Inicia a aplicação JavaFX
         launch(args);
     }
-
 }
