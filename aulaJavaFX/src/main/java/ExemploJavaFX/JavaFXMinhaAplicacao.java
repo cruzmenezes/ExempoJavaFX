2c9a95b1066232d20f9a56e7546a8f3d02761590
cruzmenezes <cruz.menezes@outlook.com>
2025-08-15 17:00:44 -0300

    configuração do ambiente

--- aulaJavaFX/src/main/java/ExemploJavaFX/JavaFXMinhaAplicacao.java
+++ aulaJavaFX/src/main/java/ExemploJavaFX/JavaFXMinhaAplicacao.java
@@ -1,17 +1,28 @@
 package ExemploJavaFX;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class JavaFXMinhaAplicacao extends Application{
+public class JavaFXMinhaAplicacao extends Application {
 
     @Override
-    public void start(Stage palco){
-        palco.setTitle("meu primeiro palco");
+    public void start(Stage palco) {
+        // Criando um texto para exibir
+        Label mensagem = new Label("Hello World!");
+
+        // Criando a cena com largura e altura
+        Scene cena = new Scene(mensagem, 300, 200);
+
+        // Configurando o palco
+        palco.setTitle("Meu Primeiro Palco");
+        palco.setScene(cena);
         palco.show();
-
-    }
-    public static void main(String[] args){
-        launch(args);
     }
 
+    public static void main(String[] args) {
+        launch(args); // Inicia a aplicação JavaFX
+    }
 }
+
